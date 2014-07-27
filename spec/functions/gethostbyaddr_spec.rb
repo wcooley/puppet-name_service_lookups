564c7b51d1148a2f52e801c9db5ccb81c21a9b29
Wil Cooley <wcooley@nakedape.cc>
2014-07-27 16:13:16 -0700

    Stub Socket.gethostbyname for sanity/portability

--- spec/functions/gethostbyaddr_spec.rb
+++ spec/functions/gethostbyaddr_spec.rb
@@ -1,13 +1,16 @@
 require 'spec_helper'
 
 describe 'gethostbyaddr' do
-  context 'basic' do
-    it do
-      should run.with_params('127.0.0.1').and_return({
-        'name' => 'localhost',
-        'aliases' => ['1.0.0.127.in-addr.arpa'],
-        'address' => '127.0.0.1',
-      })
-    end
+  it 'should run and return stubbed results' do
+    Socket.stubs(:gethostbyaddr).returns([
+        "slocalhost", ["localhost.localdomain"],
+        2, [127,0,0,2].pack("CCCC")
+      ])
+    expected_result = {
+      'name'    => 'slocalhost',
+      'aliases' => ['localhost.localdomain'],
+      'address' => '127.0.0.2',
+    }
+    expect(subject).to run.with_params('127.0.0.2').and_return(expected_result)
   end
 end
