3614510d94b664226e1794bbfb37a9cc460472b2
Wil Cooley <wcooley@nakedape.cc>
2014-07-27 16:47:15 -0700

    Stub Socket.gethostbyname for sanity

--- spec/functions/gethostbyname_spec.rb
+++ spec/functions/gethostbyname_spec.rb
@@ -1,13 +1,17 @@
 require 'spec_helper'
 
 describe 'gethostbyname' do
-  context 'basic' do
-    it do
-      should run.with_params('localhost').and_return({
-        'name' => 'localhost',
-        'aliases' => [],
-        'address' => '127.0.0.1',
-      })
-    end
+  it 'should run and return stubbed results' do
+    Socket.stubs(:gethostbyname).returns([
+      'slocalhost', ['localhost.localdomain'],
+      2, [127, 0, 0, 2].pack('CCCC')
+    ])
+    expected_result = {
+      'name' => 'slocalhost',
+      'aliases' => ['localhost.localdomain'],
+      'address' => '127.0.0.2'
+    }
+    expect(subject).to run.with_params('localhost').and_return(expected_result)
+  end
   end
 end
