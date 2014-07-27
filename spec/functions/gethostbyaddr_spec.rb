09495cb7300c083553a69708eb2352fe8ba7fc31
Wil Cooley <wcooley@nakedape.cc>
2014-07-27 16:19:36 -0700

    Add encoding, other tweaks per style guide

--- spec/functions/gethostbyaddr_spec.rb
+++ spec/functions/gethostbyaddr_spec.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8
+
 require 'spec_helper'
 
 describe 'gethostbyaddr' do
@@ -8,13 +10,13 @@ describe 'gethostbyaddr' do
 
   it 'should run and return stubbed results' do
     Socket.stubs(:gethostbyaddr).returns([
-        "slocalhost", ["localhost.localdomain"],
-        2, [127,0,0,2].pack("CCCC")
-      ])
+      'slocalhost', ['localhost.localdomain'],
+      2, [127, 0, 0, 2].pack('CCCC')
+    ])
     expected_result = {
       'name'    => 'slocalhost',
       'aliases' => ['localhost.localdomain'],
-      'address' => '127.0.0.2',
+      'address' => '127.0.0.2'
     }
     expect(subject).to run.with_params('127.0.0.2').and_return(expected_result)
   end
