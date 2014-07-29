fbb22baf88dcc38f7ee41e9bca3890e0e5cef2d9
Wil Cooley <wcooley@nakedape.cc>
2014-07-28 22:45:19 -0700

    Implement `getpwuid` lookup function
    FIXME: Refactor this and `getpwnam` to be more DRY.

---
+++ lib/puppet/parser/functions/getpwuid.rb
@@ -0,0 +1,31 @@
+# encoding: UTF-8
+
+require 'etc'
+
+module Puppet::Parser::Functions
+  newfunction(:getpwuid, :type => :rvalue) do |args|
+
+    user = args[0]
+
+    begin
+      pwent = Etc.getpwuid(user)
+
+      # Truncate to first 7 for consistency between platforms
+      pwvals = pwent.values[0..6]
+
+      # In Ruby 1.8, members returns strings but in 1.9 returns symbols
+      pwkeys = pwent.members[0..6].collect { |k| k.to_s }
+
+      result = Hash[pwkeys.zip(pwvals)]
+
+    rescue ArgumentError => ae
+      if ae.to_s =~ /can't find user/
+       result = :undef
+      else
+        raise ae
+      end
+    end
+
+    result
+  end
+end
---
+++ spec/functions/getpwuid_spec.rb
@@ -0,0 +1,63 @@
+# encoding: UTF-8
+
+require 'spec_helper'
+
+describe 'getpwuid' do
+
+  it 'should exist' do
+    Puppet::Parser::Functions.function('getpwuid') \
+      .should eq 'function_getpwuid'
+  end
+
+  context 'should run and return stubbed results' do
+    pwent_ars = [
+        ['name', 'jensenb'],
+        ['passwd', '********'],
+        ['uid', 501],
+        ['gid', 101],
+        ['gecos', 'Babs Jensen'],
+        ['dir', '/home/jensenb'],
+        ['shell', '/bin/tcsh']
+    ]
+    pwent_vals = pwent_ars.collect { |k,v| v }
+    pwent_hash = Hash[pwent_ars]
+
+    pwent_struct = Struct::Passwd[*pwent_vals]
+
+    before(:each) do
+      Etc.stubs(:getpwuid).returns(pwent_struct)
+    end
+
+    # The failure message is buggy in rspec-puppet 1.0.1
+    # and not as readable as the default formatter when it does work
+    it 'with rspec-puppet' do
+      should(run.with_params(501).and_return(pwent_hash))
+    end
+
+    # Ruby 1.8.7 barfs with
+    #   uninitialized constant RSpec::Expectations::Differ::Encoding
+    it('using Puppet function directly', :if => RUBY_VERSION >= '1.9.0') do
+      scope = PuppetlabsSpec::PuppetInternals.scope
+      expect(scope.function_getpwuid([501])).to eq(pwent_hash)
+    end
+  end
+
+  it('should lookup UID 0 unstubbed', :slow => true) do
+      scope = PuppetlabsSpec::PuppetInternals.scope
+      result = scope.function_getpwuid([0])
+      expect(result['name']).to eq('root')
+      expect(result['uid']).to eq(0)
+  end
+
+  it 'should return undef on lookup failure' do
+    # Use 'root' to confirm that stubbed exception is working
+    Etc.stubs(:getpwuid).raises(ArgumentError, "can't find user for root")
+    expect(subject).to run.with_params(0).and_return(:undef)
+  end
+
+  it 'should re-raise other exceptions' do
+    Etc.stubs(:getpwuid).raises(ArgumentError, 'not the right argument')
+    expect(subject).to run.with_params(0)\
+      .and_raise_error(ArgumentError, 'not the right argument')
+  end
+end
