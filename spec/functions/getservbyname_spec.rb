85dea75375211f0fd6f5dde80870d1a1075e7a06
Wil Cooley <wcooley@nakedape.cc>
2014-08-01 00:56:17 -0700

    Add check for presence

--- spec/functions/getservbyname_spec.rb
+++ spec/functions/getservbyname_spec.rb
@@ -3,6 +3,12 @@
 require 'spec_helper'
 
 describe 'getservbyname' do
+
+  it 'should exist' do
+    Puppet::Parser::Functions.function('getservbyname') \
+      .should eq 'function_getservbyname'
+  end
+
   context 'basic' do
     it '' do
       should run.with_params('telnet').and_return(23)
