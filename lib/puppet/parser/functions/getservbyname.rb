7bf11cbe8638e203966984f52e26284e995acc1f
Wil Cooley <wcooley@nakedape.cc>
2014-08-07 00:12:36 -0700

    Use unpacking instead of hand-holding arguments

--- lib/puppet/parser/functions/getservbyname.rb
+++ lib/puppet/parser/functions/getservbyname.rb
@@ -5,10 +5,7 @@ require 'socket'
 module Puppet::Parser::Functions
   newfunction(:getservbyname, :type => :rvalue) do |args|
 
-    serv = args[0]
-    proto = args[1]
-
-    result = Socket.getservbyname(serv, proto)
+    result = Socket.getservbyname(*args)
 
     result
   end
